5faed252c753cb6bf72afe7c5a85c059359158a9
ma3llim007 <mohdsameer68257@gmail.com>
2024-01-31 00:23:22 +0530

    Calculator Project Add

---
+++ 02-JavaScript-Project/09_Calculator/script.js
@@ -0,0 +1,25 @@
+let string = "";
+const buttons = document.querySelectorAll(".buttons");
+const InputFeild = document.getElementById("InputFeild");
+Array.from(buttons).forEach((button) => {
+    button.addEventListener("click", (e) => {
+        InputData = e.originalTarget.value;
+        RemoveDel = InputData.replace("Del", "");
+        string += RemoveDel;
+        if (InputData == 'AC') {
+            InputFeild.value = '';
+            string = '';
+        } else if (InputData == 'Del') {
+            string = string.slice(0, - 1);
+        } else if (InputData == '=') {
+            string = string.replace("=", "");
+            string = eval(string);
+        } else if (InputData == '^') {
+            string = string.replace("^", "");
+            string = Math.pow(string, 2);
+        }
+        
+        // Show the result and number in input
+        InputFeild.value = string;    
+    });
+});
