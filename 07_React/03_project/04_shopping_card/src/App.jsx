3e947276adf48a879754ef125ff40fda44877199
ma3llim007 <mohdsameer68257@gmail.com>
2024-07-17 20:16:56 +0530

    chore: 4 shopping card-Project

---
+++ 07_React/03_project/04_shopping_card/src/App.jsx
@@ -0,0 +1,60 @@
+import { useState } from 'react'
+import Header from './components/Header'
+import Shop from './components/Shop';
+import { ToastContainer, toast } from 'react-toastify';
+import 'react-toastify/dist/ReactToastify.css';
+import CardList from './components/CardList';
+
+function App() {
+  const [cart, setCart] = useState([]);
+  const [show, setShow] = useState(true);
+
+  const handleAddToCart = (data) => {
+    let isPrevent = false;
+    cart.forEach(item => {
+      if (item.id === data.id) {
+        isPrevent = true;
+      }
+    });
+    if (!isPrevent) {
+      setCart([...cart, data])
+      toast.success('Item is Add To Cart', {
+        position: "bottom-center",
+        autoClose: 1300,
+        hideProgressBar: false,
+        closeOnClick: true,
+        pauseOnHover: true,
+        draggable: true,
+        progress: undefined,
+        theme: "dark",
+      })
+    } else {
+      toast.error('Item Is Already In Cart', {
+        position: "bottom-center",
+        autoClose: 1300,
+        hideProgressBar: false,
+        closeOnClick: true,
+        pauseOnHover: true,
+        draggable: true,
+        progress: undefined,
+        theme: "dark",
+      });
+    }
+  }
+
+  const handleShow = () => {
+    setShow(!show);
+  }
+
+  return (
+    <>
+      <Header cartSize={cart.length} handleShow={handleShow} />
+      {
+        show ? <Shop handleAddToCart={handleAddToCart} /> : <CardList cart={cart} setCart={setCart} />
+      }
+      <ToastContainer limit={3} />
+    </>
+  )
+}
+
+export default App;
